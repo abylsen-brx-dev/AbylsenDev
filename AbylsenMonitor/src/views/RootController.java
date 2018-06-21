cfe55f81828bc2b1c204dcfec7f9e41834f5ee15
abylsenDev <abylsen.dev@gmail.com>
2018-06-21 15:34:59 +0200

    Add the main RootController implementation (with tabs)

--- AbylsenMonitor/src/views/RootController.java
+++ AbylsenMonitor/src/views/RootController.java
@@ -2,42 +2,40 @@ package views;
 
 import java.io.IOException;
 
-import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
 
-public class RootController extends VBox {
+public class RootController extends BorderPane {
 	@FXML
-	private TextField textField;
+	private TabPane tabPane;
 
 	public RootController() {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/RootView.fxml"));
 		fxmlLoader.setRoot(this);
 		fxmlLoader.setController(this);
-
+		
 		try {
+			//Load the view.
 			fxmlLoader.load();
+			
+			//Init tabs.
+			for(Tab t : tabPane.getTabs()) {
+				initTab(t);
+			}
+			
 		} catch (IOException exception) {
 			throw new RuntimeException(exception);
 		}
 	}
-
-	public String getText() {
-		return textProperty().get();
-	}
-
-	public void setText(String value) {
-		textProperty().set(value);
-	}
-
-	public StringProperty textProperty() {
-		return textField.textProperty();
-	}
-
-	@FXML
-	protected void doSomething() {
-		System.out.println("The button was clicked!");
+	
+	private void initTab(Tab t) {
+		if(t == null)
+			return;
+		
+		switch(t.getText()) {
+		}
 	}
 }
