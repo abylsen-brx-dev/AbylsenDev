24b21371c745b1ade0a5c024ac4b1396bf3fc9bd
abylsenDev <abylsen.dev@gmail.com>
2018-06-25 22:14:29 +0200

    Add and test post method.

--- AbylsenApi/src/main/java/controller/PersonController.java
+++ AbylsenApi/src/main/java/controller/PersonController.java
@@ -3,7 +3,9 @@ package controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import model.Person;
@@ -21,5 +23,13 @@ public class PersonController {
 	@RequestMapping("/{id}")
 	public Person getPerson(@PathVariable("id") int id) {
 		return PersonService.getInstance().getPerson(id);
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public boolean update(@RequestBody Person p) {
+	    if(p == null)
+	    	return false;
+	    
+	    return PersonService.getInstance().addPerson(p);
 	}
 }
